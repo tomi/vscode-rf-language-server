0a440c5bf9bbcd2f7b465e7f661b495d109cd824
Tomi Turtiainen <tomi.turtiainen@gmail.com>
2017-01-06 19:18:46 +0200

    Refactor settings table tests

--- server/src/parser/test/parser.test.ts
+++ server/src/parser/test/parser.test.ts
@@ -8,35 +8,87 @@ import {
 
 import * as chai from "chai";
 
-const settings = `
-*** Settings ***
-Resource            resources/\${ENVIRONMENT}.robot
-Resource            resources/smoke_resources.robot
-Suite Setup         Open Default Browser
-Suite Teardown      Close Browser
-Test Setup          Navigate To Frontpage
-`;
-
 const parser = new FileParser();
 
+function parseAndAssert(stringToParse: string, expected: any) {
+  const actual = parser.parse(stringToParse);
+
+  chai.assert.deepEqual(actual, expected);
+}
+
+/**
+ *
+ */
+function createSettingsTable(content): TestDataFile {
+  const testDataFile = new TestDataFile();
+
+  testDataFile.settingsTable = Object.assign(new SettingsTable(), content);
+
+  return testDataFile;
+}
+
+/**
+ *
+ */
+function generateSettingsTableTest(tableDefinition: string, expectedData) {
+  const inputData = `*** Settings ***\n${ tableDefinition }`;
+
+  const expected = createSettingsTable(expectedData);
+
+  parseAndAssert(inputData, expected);
+}
+
 describe("RF Parser", () => {
-  it("should parse settings table", () => {
-    const settingsTable = new SettingsTable();
 
-    settingsTable.suiteSetup    = new SingleValueSetting("Suite Setup", "Open Default Browser");
-    settingsTable.suiteTeardown = new SingleValueSetting("Suite Teardown", "Close Browser");
-    settingsTable.testSetup     = new SingleValueSetting("Test Setup", "Navigate To Frontpage");
+  describe("Parsing Settings table", () => {
 
-    settingsTable.imports = [
+    it("should recognize a settings table", () => {
+      const expected = createSettingsTable({});
+
+      parseAndAssert(`*** Settings ***`, expected);
+      parseAndAssert(`*** Settings`, expected);
+      parseAndAssert(`*Settings`, expected);
+    });
+
+    it("should parse resource imports", () => {
+      generateSettingsTableTest(`
+Resource   resources/\${ENVIRONMENT}.robot
+Resource   resources/smoke_resources.robot
+`, {
+  imports: [
       new Import("Resource", "resources/\${ENVIRONMENT}.robot"),
       new Import("Resource", "resources/smoke_resources.robot"),
-    ];
+  ]
+});
+    });
 
-    const expected = new TestDataFile();
-    expected.settingsTable = settingsTable;
+    it("should parse suite setup", () => {
+      generateSettingsTableTest(
+        `Suite Setup         Open Default Browser`,
+        { suiteSetup: new Setting("Suite Setup", "Open Default Browser") }
+      );
+    });
 
-    const actual = parser.parse(settings);
+    it("should parse suite teardown", () => {
+      generateSettingsTableTest(
+        `Suite Teardown         Open Default Browser`,
+        { suiteTeardown: new Setting("Suite Teardown", "Open Default Browser") }
+      );
+    });
 
-    chai.assert.deepEqual(actual, expected);
+    it("should parse test setup", () => {
+      generateSettingsTableTest(
+        `Test Setup         Open Default Browser`,
+        { testSetup: new Setting("Test Setup", "Open Default Browser") }
+      );
+    });
+
+    it("should parse test teardown", () => {
+      generateSettingsTableTest(
+        `Test Teardown         Open Default Browser`,
+        { testTeardown: new Setting("Test Teardown", "Open Default Browser") }
+      );
+    });
+
   });
 });

3e2f50dff2aad6695e3ff5bbc174dd0c192d25ee
Tomi Turtiainen <tomi.turtiainen@gmail.com>
2017-02-23 23:56:26 +0200

    Move variable declaration parsing to separate file

---
+++ server/src/parser/variable-parsers.ts
@@ -0,0 +1,82 @@
+import * as _ from "lodash";
+
+import { DataCell } from "./table-models";
+
+import {
+  Identifier,
+  ValueExpression,
+  VariableDeclaration,
+  ScalarDeclaration,
+  ListDeclaration,
+  DictionaryDeclaration,
+} from "./models";
+
+const variableMapping = new Map([
+  ["$", parseScalar],
+  ["@", parseList],
+  ["&", parseDictionary],
+  ["%", parseEnvironment]
+]);
+
+function getRegex() {
+  // Matches the type ($, @, % or &) and name
+  // For example:
+  // ${var} --> ["${var}", "$", "var"]
+  // @{var2} = --> ["${var2}", "@", "var2"]
+  return /([$,@,%,&]){([^}]+)}/;
+}
+
+export function isVariable(cell: DataCell) {
+  const typeAndNameRegex = getRegex();
+  const possibleTypeAndName = cell.content;
+
+  return typeAndNameRegex.test(possibleTypeAndName);
+}
+
+export function parseTypeAndName(cell: DataCell) {
+  const typeAndNameRegex = getRegex();
+  const typeAndName = cell.content;
+
+  const result = typeAndName.match(typeAndNameRegex);
+  return {
+    type: result[1],
+    name: new Identifier(result[2], cell.location)
+  };
+}
+
+export function parseVariableDeclaration(typeAndName, values: ValueExpression[], location): VariableDeclaration {
+  const { type, name } = typeAndName;
+
+  const variableParserFn = getVariableParserFn(type);
+  if (!variableParserFn) {
+    throw new Error(`Invalid variable type ${ type }`);
+  }
+
+  return variableParserFn(name, values, location);
+}
+
+function getVariableParserFn(type: string): Function {
+  const parser = variableMapping.get(type);
+
+  return parser;
+}
+
+function parseScalar(name: Identifier, values: ValueExpression[], location): VariableDeclaration {
+  const value = _.first(values);
+
+  return new ScalarDeclaration(name, value, location);
+}
+
+function parseList(name: Identifier, values: ValueExpression[], location): VariableDeclaration {
+  return new ListDeclaration(name, values, location);
+}
+
+function parseDictionary(name: Identifier, values: ValueExpression[], location): VariableDeclaration {
+  // TODO
+  return new DictionaryDeclaration(name, null, location);
+}
+
+function parseEnvironment(name: Identifier, values: ValueExpression[], location): VariableDeclaration {
+  // TODO
+  return;
+}
--- server/src/parser/variables-table-parser.ts
+++ server/src/parser/variables-table-parser.ts
@@ -5,24 +5,15 @@ import {
   DataRow
 } from "./table-models";
 
-import {
-  Identifier,
-  ValueExpression,
-  VariablesTable,
-  ScalarDeclaration,
-  ListDeclaration,
-} from "./models";
+import { VariablesTable } from "./models";
+
+import { parseValueExpression } from "./primitive-parsers";
 
 import {
-  parseValueExpression
-} from "./primitive-parsers";
-
-const variableMapping = new Map([
-  ["$", parseScalar],
-  ["@", parseList],
-  ["&", parseDictionary],
-  ["%", parseEnvironment]
-]);
+  isVariable,
+  parseTypeAndName,
+  parseVariableDeclaration
+} from "./variable-parsers";
 
 export function parseVariablesTable(dataTable: DataTable): VariablesTable {
   const variablesTable = new VariablesTable(dataTable.location);
@@ -37,63 +28,13 @@ function parseRow(variablesTable: VariablesTable, row: DataRow) {
     return;
   }
 
-  const typeAndName = parseTypeAndName(row);
-  if (!typeAndName) {
+  const typeNameCell = row.first();
+  if (!isVariable(typeNameCell)) {
     return;
   }
 
-  const { type, name } = typeAndName;
-
-  const variableParserFn = getVariableParserFn(type);
-
+  const typeAndName = parseTypeAndName(typeNameCell);
   const values = row.getCellsByRange(1).map(parseValueExpression);
-  variableParserFn(variablesTable, name, values, row.location);
-}
-
-function parseTypeAndName(row: DataRow) {
-  // Matches the type ($, @, % or &) and name
-  // For example:
-  // ${var} --> ["${var}", "$", "var"]
-  // @{var2} = --> ["${var2}", "@", "var2"]
-  const typeAndNameRegex = /([$,@,%,&]){([^}]+)}/;
-  const typeNameCell = row.first();
-  const typeAndName = typeNameCell.content;
-
-  if (!typeAndNameRegex.test(typeAndName)) {
-    return null;
-  }
-
-  const result = typeAndName.match(typeAndNameRegex);
-  return {
-    type: result[1],
-    name: new Identifier(result[2], typeNameCell.location)
-  };
-}
-
-function getVariableParserFn(type: string) {
-  const parser = variableMapping.get(type);
-
-  return parser || _.noop;
-}
-
-function parseScalar(variablesTable: VariablesTable, name: Identifier, values: ValueExpression[], location) {
-  const value = _.first(values);
-
-  const variable = new ScalarDeclaration(name, value, location);
-
-  variablesTable.addVariable(variable);
-}
-
-function parseList(variablesTable: VariablesTable, name: Identifier, values: ValueExpression[], location) {
-  const variable = new ListDeclaration(name, values, location);
-
-  variablesTable.addVariable(variable);
-}
-
-function parseDictionary(variablesTable: VariablesTable, name: Identifier, values: ValueExpression[], location) {
-  // TODO
-}
-
-function parseEnvironment(variablesTable: VariablesTable, name: Identifier, values: ValueExpression[], location) {
-  // TODO
+  const variableDeclaration = parseVariableDeclaration(typeAndName, values, row.location);
+  variablesTable.addVariable(variableDeclaration);
 }

4151aa05ac397cb7f299f44937dbed22eb0c9f79
Tomi Turtiainen <tomi.turtiainen@gmail.com>
2017-03-04 14:03:06 +0200

    Guard type-guards against undefined parameter

--- server/src/intellisense/type-guards.ts
+++ server/src/intellisense/type-guards.ts
@@ -14,47 +14,47 @@ import {
 } from "../parser/models";
 
 export function isIdentifier(node: Node): node is Identifier {
-  return (<Identifier>node).type === "Identifier";
+  return node && (<Identifier>node).type === "Identifier";
 }
 
 export function isVariableExpression(node: Node): node is VariableExpression {
-  return (<VariableExpression>node).type === "VariableExpression";
+  return node && (<VariableExpression>node).type === "VariableExpression";
 }
 
 export function isCallExpression(node: Node): node is CallExpression {
-  return (<CallExpression>node).type === "CallExpression";
+  return node && (<CallExpression>node).type === "CallExpression";
 }
 
 export function isScalarDeclaration(node: Node): node is ScalarDeclaration {
-  return (<ScalarDeclaration>node).type === "ScalarDeclaration";
+  return node && (<ScalarDeclaration>node).type === "ScalarDeclaration";
 }
 
 export function isListDeclaration(node: Node): node is ListDeclaration {
-  return (<ListDeclaration>node).type === "ListDeclaration";
+  return node && (<ListDeclaration>node).type === "ListDeclaration";
 }
 
 export function isDictionaryDeclaration(node: Node): node is DictionaryDeclaration {
-  return (<DictionaryDeclaration>node).type === "DictionaryDeclaration";
+  return node && (<DictionaryDeclaration>node).type === "DictionaryDeclaration";
 }
 
 export function isVariableDeclaration(node: Node): node is VariableDeclaration {
-    return isScalarDeclaration(node) ||
+    return node && isScalarDeclaration(node) ||
       isListDeclaration(node) ||
       isDictionaryDeclaration(node);
 }
 
 export function isStep(node: Node): node is Step {
-  return (<Step>node).type === "Step";
+  return node && (<Step>node).type === "Step";
 }
 
 export function isUserKeyword(node: Node): node is UserKeyword {
-  return (<UserKeyword>node).type === "UserKeyword";
+  return node && (<UserKeyword>node).type === "UserKeyword";
 }
 
 export function isTestCase(node: Node): node is TestCase {
-  return (<TestCase>node).type === "TestCase";
+  return node && (<TestCase>node).type === "TestCase";
 }
 
 export function isFunctionDeclaration(node: Node): node is FunctionDeclaration {
-  return isUserKeyword(node) || isTestCase(node);
+  return node && isUserKeyword(node) || isTestCase(node);
 }

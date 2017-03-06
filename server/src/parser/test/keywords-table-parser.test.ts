10d831a3faf4f690fa8e3a5e737aa52f0bf038a6
Tomi Turtiainen <tomi.turtiainen@futurice.com>
2017-03-06 22:44:15 +0200

    Fix keywords table parser tests

--- server/src/parser/test/keywords-table-parser.test.ts
+++ server/src/parser/test/keywords-table-parser.test.ts
@@ -1,13 +1,18 @@
 import * as _ from "lodash";
 import * as chai from "chai";
 
-import { parseKeywordsTable } from "../keywords-table-parser";
+import { FileParser } from "../parser";
+const parser = new FileParser();
+
 import {
   KeywordsTable,
   UserKeyword,
   Step,
   CallExpression,
-  Identifier
+  Identifier,
+  Literal,
+  VariableExpression,
+  VariableKind
 } from "../models";
 
 import {
@@ -18,8 +23,8 @@ import {
   cell
 } from "./test-helper";
 
-function parseAndAssert(tableDefinition, expected) {
-  const actual = parseKeywordsTable(tableDefinition);
+function parseAndAssert(tableDefinition: string, expected: KeywordsTable) {
+  const actual = parser.parseFile(tableDefinition).keywordsTable;
 
   chai.assert.deepEqual(actual, expected);
 }
@@ -28,62 +33,67 @@ function keywordsTable(location, keywords) {
   return Object.assign(new KeywordsTable(location), { keywords });
 }
 
-function keyword(startPosition, name, steps) {
+function keyword(startPosition, name: Identifier, steps: Step[]) {
   return Object.assign(new UserKeyword(name, startPosition), { steps });
 }
 
 describe("Parsing Keywords table", () => {
 
   it("should skip invalid data", () => {
-    const tableDefinition = table("Keywords", {
-      header: row(location(0, 0, 0, 10)),
-      rows: [
-        row(location(1, 0, 1, 10), [
-          cell(location(2, 0, 2, 10), ""),
-          cell(location(1, 0, 1, 10), "not a keyword"),
-          cell(location(1, 0, 1, 10), "cell2")
-        ]),
-        row(location(2, 0, 2, 10), [
-          cell(location(2, 0, 2, 10), ""),
-          cell(location(2, 0, 2, 10), "!another invalid"),
-          cell(location(2, 0, 2, 10), "data")
-        ]),
-      ]
-    });
+    const data =
+`*** Keywords ***
+    not a keyword   cell2
+      !another invalid    data
+`;
+    const expected = keywordsTable(location(0, 0, 3, 0), []);
 
-    const expected = keywordsTable(location(0, 0, 2, 10), []);
-
-    parseAndAssert(tableDefinition, expected);
+    parseAndAssert(data, expected);
   });
 
   it("should parse keyword", () => {
-    const tableDefinition = table("Keywords", {
-      header: row(location(0, 0, 0, 10)),
-      rows: [
-        row(location(1, 0, 1, 10), [cell(location(1, 0, 1, 10), "Keyword Name")]),
-        row(location(2, 0, 2, 10), [
-          cell(location(2, 0, 2, 10), ""),
-          cell(location(2, 0, 2, 10), "Step 1"),
-          cell(location(2, 0, 2, 10), "arg1"),
-          cell(location(2, 0, 2, 10), "arg2"),
-        ]),
-        row(location(3, 0, 3, 10), [
-          cell(location(3, 0, 3, 10), ""),
-          cell(location(3, 0, 3, 10), "Step 2"),
-          cell(location(3, 0, 3, 10), "${VAR}"),
-          cell(location(3, 0, 3, 10), "more complex arg"),
-        ]),
-      ]
-    });
+    const data =
+`*** Keywords ***
+Keyword Name
+    Step 1    arg1      arg2
+    Step 2    \${VAR}    a longer arg2
+`;
 
-    const expected = keywordsTable(location(0, 0, 3, 10), [
-      keyword(position(1, 0), "Keyword Name", [
-        // new Step(new CallExpression(new Identifier() "Step 1", ["arg1", "arg2"], location(2, 0, 2, 10)),
-        // new Step("Step 2", ["${VAR}", "more complex arg"], location(3, 0, 3, 10)),
-      ])
+    const expected = keywordsTable(location(0, 0, 4, 0), [
+      keyword(
+        position(1, 0),
+        new Identifier("Keyword Name", location(1, 0, 1, 12)),
+        [
+          new Step(
+            new CallExpression(
+              new Identifier("Step 1", location(2, 4, 2, 10)),
+              [
+                new Literal("arg1", location(2, 14, 2, 18)),
+                new Literal("arg2", location(2, 24, 2, 28)),
+              ],
+              location(2, 4, 2, 28)
+            ),
+            location(2, 0, 2, 28)
+          ),
+          new Step(
+            new CallExpression(
+              new Identifier("Step 2", location(3, 4, 3, 10)),
+              [
+                new VariableExpression(
+                  new Identifier("VAR", location(3, 16, 3, 19)),
+                  "Scalar",
+                  location(3, 14, 3, 20)
+                ),
+                new Literal("a longer arg2", location(3, 24, 3, 37)),
+              ],
+              location(3, 4, 3, 37)
+            ),
+            location(3, 0, 3, 37)
+          ),
+        ]
+      )
     ]);
 
-    parseAndAssert(tableDefinition, expected);
+    parseAndAssert(data, expected);
   });
 
 });

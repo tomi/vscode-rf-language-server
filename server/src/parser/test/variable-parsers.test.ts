8d0dab54f28b06b42e6b47d57dbaebcb8d3c9ffc
Tomi Turtiainen <tomi.turtiainen@gmail.com>
2017-06-18 20:58:20 +0300

    Add tests for variable parser

---
+++ server/src/parser/test/variable-parsers.test.ts
@@ -0,0 +1,62 @@
+import * as _ from "lodash";
+import * as chai from "chai";
+
+import { isVariable, parseTypeAndName } from "../variable-parsers";
+import { DataCell } from "../table-models";
+
+import {
+  Identifier,
+  ScalarDeclaration,
+  TemplateElement,
+  TemplateLiteral,
+  Step
+} from "../models";
+
+import { location } from "./test-helper";
+
+const DUMMY_LOC = location(0, 0, 0, 0);
+
+function assertIsVariable(cellData: string) {
+  const cell = new DataCell(cellData, DUMMY_LOC);
+  const actual = isVariable(cell);
+
+  chai.assert.isTrue(actual);
+}
+
+function assertParseResult(cellData: string, expectedType, expectedName) {
+  const cell = new DataCell(cellData, DUMMY_LOC);
+  const expected = {
+    type: expectedType,
+    name: new Identifier(expectedName, DUMMY_LOC)
+  };
+
+  const actual = parseTypeAndName(cell);
+
+  chai.assert.deepEqual(actual, expected);
+}
+
+describe("Variable parsing", () => {
+  describe("isVariable", () => {
+    it("should recognize an empty scalar", () => {
+      assertIsVariable("${}");
+    });
+
+    it("should recognize an empty list", () => {
+      assertIsVariable("${}");
+    });
+
+    it("should recognize scalar", () => {
+      assertIsVariable("${var}");
+    });
+
+    it("should recognize scalar", () => {
+      assertIsVariable("${var}");
+    });
+  });
+
+  describe("parseTypeAndName", () => {
+    it("should parse empty scalar", () => {
+      assertParseResult("${}", "$", "");
+    });
+  });
+});

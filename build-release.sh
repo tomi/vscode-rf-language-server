d0e6abba08faa2fc6b12065c23d38a9afaa9a2ce
Tomi Turtiainen <tomi.turtiainen@futurice.com>
2019-07-13 01:42:57 +0300

    Update build release script

--- build-release.sh
+++ build-release.sh
@@ -2,9 +2,13 @@
 
 cd server
 echo "Building server"
+rm -rf ./node_modules
+npm ci
 npm run clean
 npm run compile
 
 echo "Building client"
 cd ../client
+rm -rf ./node_modules
+npm ci
 vsce package
